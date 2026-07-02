ba2c5c5bf63e90a016a9e726abc902c7ee92c14c
lvwpacifica <lvwpacifica@gmail.com>
2026-07-02 15:28:51 -0500

    main.py

--- src/main.py
+++ src/main.py
@@ -3,17 +3,23 @@ from transform import transform_weather
 from load import init_db, insert_weather
 from aggregate_spark import aggregate_weather_records
 
+# Execute the end-to-end weather data pipeline
 def run_pipeline():
+    # Initialize the PostgreSQL database
     init_db()
-
+    
+    # Extract weather data from the API
     raw_data = fetch_weather()
 
+    # Store the raw JSON locally and upload it to Azure Data Lake Storage Gen2
     filename = save_weather(raw_data)
     upload_to_azure(filename)
 
+    # Transform the raw data and load it into PostgreSQL
     cleaned_data = transform_weather(raw_data)
     insert_weather(cleaned_data)
 
+    # Generate daily aggregated weather metrics using PySpark
     aggregate_weather_records()
 
     print("Uploaded raw weather JSON to Azure:", filename)
